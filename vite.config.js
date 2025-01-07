f8055e279c9713522338253c89b2a60ea648710f
Yashraj Admane <76515550+devYashraj@users.noreply.github.com>
2025-01-07 13:44:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define:{
-    baseUrl: '"https://acoserver-devyashraj.onrender.com/api/"'
+    baseUrl: '"https://localhost:3000/api/"'
   }
 })
